200b06432b5710ed262d5bf768f5be208e374c48
Chersie225 <ivansupr225@gmail.com>
2020-10-29 23:48:15 +0300

    Вывод на 2 лампочки с двух датчиков

--- software/main/main.ino
+++ software/main/main.ino
@@ -1,9 +1,23 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(D5, INPUT);//Датчик CO2
+  pinMode(D2,INPUT);//Датчик температуры
+  pinMode(D4,OUTPUT);//Первый светодиод указывающий на уровень CO2
+  pinMode(D3,OUTPUT);//Второй светодиод указывающий на температуру
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int t = digitalRead(D5);// wait for a second
+  int y = digitalRead(D2);
+  if(t>500){
+    digitalWrite(D4,HIGH);
+    }
+    else{
+      digitalWrite(D4,LOW);
+    }
+    if(y>25){
+      digitalWrite(D3,HIGH);
+    }
+    else{
+      digitalWrite(D3,LOW);
+    }
+  
 }
